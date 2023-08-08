94950a903be656c83035b8935f33814fb9b08d90
xiao.zhang <xiao.zhang@mobvista.com>
2023-08-08 17:39:44 +0800

    初始化提交

---
+++ src/main/java/com/zx/test_jenkins/TestJenkinsApplication.java
@@ -0,0 +1,13 @@
+package com.zx.test_jenkins;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestJenkinsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestJenkinsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zx/test_jenkins/TestJenkinsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zx.test_jenkins;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestJenkinsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
